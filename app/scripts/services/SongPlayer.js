a85af037aea4e040ad15323b636dd075df51fce3
dvilailath <dvilailath@yahoo.com>
2017-06-27 23:13:18 -0500

    assignment 7 services pt.2: done

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,7 +2,10 @@
      function SongPlayer() {
          var SongPlayer = {};
          
-         
+         /**
+         * @desc set Current song to play
+         * @type {Object}
+         */
          var currentSong = null;
          /**
          * @desc Buzz object audio file
@@ -10,6 +13,11 @@
          */
          var currentBuzzObject = null;
          
+         /**
+         * @function SongPlayer.play
+         * @desc Plays a song after checking to see if anything is playing or stopped.
+         * @param {Object} song
+         */
          SongPlayer.play = function(song) {
              if (currentSong !== song) {
              /**
@@ -32,15 +40,31 @@
              };
              
              setSong(song);
-             currentBuzzObject.play(); 
-             song.playing = true;
+             playSong(song);
             }
          };
          
-          SongPlayer.pause = function(song) {
+         /**
+         * @function SongPlayer.pause
+         * @desc Pauses a song that is playing and stops currently playing song and currentBuzzObject;
+         * @param {Object} song
+         */
+         SongPlayer.pause = function(song) {
              currentBuzzObject.pause();
              song.playing = false;
          };
+         
+         /**
+         * @function playSong
+         * @desc Plays a song that is paused and resumes currently playing song and currentBuzzObject;
+         * @param {Object} song
+         */
+         
+         playSong = function(song) {
+             currentBuzzObject.play();
+             song.playing = true;
+         }
+          
          
          return SongPlayer;
      }
